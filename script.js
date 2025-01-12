9c0789d7c71781cd3294c10e8f20b0dc5e9f8953
Reda Elemawy <redaelemawy98@gmail.com>
2025-01-12 18:22:58 +0200

    add moasory layout styles and js for row and col span

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function generateGalleryItems(page) {
 
     // Create an image with a random height
     const img = document.createElement('img');
-    const randomHeight = Math.floor(Math.random() * (400 - 200 + 1)) + 200; // Random height between 200px and 400px
+    const randomHeight = Math.floor(Math.random() * (500 + 1)) + 200; // Random height between 200px and 400px
     img.src = `https://picsum.photos/200/${randomHeight}?random=${page * i}`;
     img.alt = `Image ${page * i} with height ${randomHeight}`;
 
@@ -19,12 +19,31 @@ function generateGalleryItems(page) {
   }
   return items;
 }
+function setRowSpans() {
+  const items = document.querySelectorAll('.gallery-item');
+  console.log(items);
 
+  items.forEach((item) => {
+    const img = item.querySelector('img');
+    console.log(img);
+
+    // Wait for image to load to get correct height
+    img.onload = () => {
+      console.log('loaded');
+
+      const rowSpan = Math.ceil(Math.random() * 8) + 1; // Random span between 1 and 4
+      const columnSpan = Math.ceil(Math.random() * 2);
+      item.style.setProperty('--row-span', rowSpan);
+      item.style.setProperty('--column-span', columnSpan);
+    };
+  });
+}
 // Function to load images
 function loadImages() {
   const items = generateGalleryItems(page);
   items.forEach((item) => gallery.appendChild(item));
   page++;
+  // setRowSpans();
 }
 
 // Infinite scrolling logic
